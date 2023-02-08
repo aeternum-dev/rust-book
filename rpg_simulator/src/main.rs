9d879382d61815e01085bf2b29977eccf5c7d222
aeternum-dev <viccesvok4@gmail.com>
2023-02-08 23:13:54 +0100

    add Inventory, Player structs

--- rpg_simulator/src/main.rs
+++ rpg_simulator/src/main.rs
@@ -1,27 +1,66 @@
+//declarations
+use std::io;
 use std::thread::sleep;
 use std::time::Duration;
 
-//battle_sim(hp, dmg, arm, hp_boss, dmg_boss, arm_boss)
-//  while true
-//      hp_boss = hp_boss-(dmg-arm_boss) //OR IF ARMOR IS TOO HIGH
-//      if hp_boss < 1:
-//          break
-//      hp = hp-(dmg_boss-arm)
-//      if hp_boss < 1:
-//          break
+//structs for for different mechanisms
+
+//tuple structs for item types
+struct Armor (String, i8 );
+struct Weapon (String, i8 );
+struct Ring (String, i8, i8 );
+
+//Inventory struct and implementation block
+pub struct Inventory {Armors: Vec<Armor>, Weapons: Vec<Weapon>, Rings: Vec<Ring>}
+
+impl Inventory {
+    pub fn add(&mut self, purchased_item: Armor) {
+        self.Armors.push(Armor)
+    }
+    pub fn add(&mut self, purchased_item: Weapon) {
+        self.Weapons.push(Weapon)
+    }
+    pub fn add(&mut self, purchased_item: Ring) {
+        self.Rings.push(Ring)
+    }
+    
+}
+
+//Player has an inventory element instead of inheriting Inventory
+pub struct Player {
+    hp: i8,
+    dmg: i8,
+    arm: i8,
+    player_inventory: Inventory
+}
+    
+impl Player {
+    fn new() {
+        Player {
+            hp: 10, dmg: 0, arm: 0,
+            player_inventory: Inventory { Armors: Vec::new(), Weapons: Vec::new(), Rings: Vec::new()}
+        }
+    }
 
 
-//Stuff I am going to need:
-// loop cycle
-//logical value assignment
-//let number = if condition { 5 } else { 6 };
-//cmp ordering shitto
-// if other guy's armor is greater or equal, then 1 final damage, else the difference
+    //fn recalculate_stats(&mut self.player_inventory) {
+    //    let dmg =
+    //    for element in self.player_inventory.Armors {
+    //
+    //    }
+    //}
+}
+    
+//functions
+
+//calculating damage and armor values
+//TODO
 
 
-fn battle_sim(mut hp: u8, dmg: u8, arm: u8, mut hp_boss: u8, dmg_boss: u8, arm_boss: u8) -> bool
+//simulating combat
+fn battle_sim(mut hp: i8, dmg: i8, arm: i8, mut hp_boss: i8, dmg_boss: i8, arm_boss: i8) -> bool
 {
-    let player_victorious = loop
+    let is_victorious = loop
     {
         hp_boss = if arm_boss >= dmg {hp_boss - 1} else {hp_boss - (dmg-arm_boss)};
         println!("You deal damage, the boss goes down to {} hp", hp_boss);
@@ -34,12 +73,40 @@ fn battle_sim(mut hp: u8, dmg: u8, arm: u8, mut hp_boss: u8, dmg_boss: u8, arm_b
         if hp < 1 {break false;}
     };
 
-    player_victorious
-    //and basically this loop gives us the final outcome
-    //let's
+    is_victorious
+    
 }
 
 fn main() {
-    println!("{}",battle_sim(8,5,5,12,7,2));
-    println!("Hello, world!");
+    //create shop
+    let shop_inventory = (
+        Armor (String::from("Sturdy Combat Armor"), 10),
+        Weapon (String::from("Broadsider"), 11),
+        Ring (String::from("Magic ring"), 6, 3)
+        
+    );
+    
+    //create player
+    let mut player1 = Player::new();
+        
+
+
+    //asking user for what items wants to buy
+    //TODO: Put in a for cycle
+    let mut input_string = String::new();
+    
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Failed to read line");
+        
+    let item_index: u8 = input_string.parse::<i8>().unwrap_or(-1);
+
+    //add item with the index to player's inventory
+    player1.player_inventory.add(shop_inventory.item_index);
+
+
+    //recalculate damage and armor values
+
+
+    //battle sim
 }
